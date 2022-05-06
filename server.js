5e75c4f8eb306e964bddb38d71f6def96bd36227
AlanWu888 <74883696+AlanWu888@users.noreply.github.com>
2022-05-06 11:55:20 +0100

    removed files that are not being used

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express')
 const app = express()
 const { ROLE, users } = require('./data')
 const { authUser, authRole } = require('./basicAuth')
-const projectRouter = require('./routes/projects')
 
 app.set('view engine', 'ejs')   // set view engine
 
 app.use(express.json())
 app.use(setUser)
-app.use('/projects', projectRouter)
 app.use(express.static("public"))   // diplay static pages - CSS AND JS HERE (and any pages which do not have dynamic data)
 
 app.get('/', (req, res) => {
@@ -30,6 +28,7 @@ app.get('/specialist', authUser, authRole(ROLE.SPECIALIST), (req, res) => {
 })
 
 function setUser(req, res, next) {
+  // set user to be signed in
   const userId = req.body.userId
   if (userId) {
     req.user = users.find(user => user.id === userId)
