943968768481ad0dc568799175bf4ccdbbad9e79
Alan Wu <74883696+AlanWu888@users.noreply.github.com>
2022-05-11 20:49:47 +0100

    sql fixes
    dropdown menu items read in from database when "reporting new problem", additionally made the app.js sql queries more readable. Added some comments.

--- app.js
+++ app.js
@@ -51,6 +51,8 @@ app.post('/auth', (req, res) => {
 				req.session.username = username;
 				// Redirect to home page based off userType
 				var usertype = results[0].userType
+
+				// If admin user signs in
 				if (usertype === 'admin') {
 					var sql_users = "SELECT employee_id, firstname, surname, speciality, contract_type, start_date from Users"
 					connection.query(sql_users, function (err_users, users_data, fields_users) {
@@ -59,15 +61,58 @@ app.post('/auth', (req, res) => {
 						res.render('admin', { userName: username, userData: users_data})
 					});		
 				}
+
+				// If employee user signs in
 				if (usertype === 'employee') {
 					// app.get('selfhelp' , { name: username })
-					var sql_cases = "SELECT case_id, user_id, date_opened, status_code, software_name, hardware_make_name, model_name, problem_title, solution FROM Cases as c INNER JOIN Problem as prob ON c.problem_id=prob.problem_id INNER JOIN HardwareModels as models ON c.hardware_id=models.model_id INNER JOIN HardwareMake as make ON models.make_id=make.hardware_make_id INNER JOIN Software as soft ON c.software_id=soft.software_id INNER JOIN Solutions as solu ON prob.problem_id=solu.problem_id"
+					var sql_cases = "SELECT case_id, date_opened, status_code, software_name, hardware_make_name, model_name, problem_title, solution " +
+									"FROM Cases as c " + 
+									"INNER JOIN Problem as prob " + 
+									"ON c.problem_id=prob.problem_id " + 
+									"INNER JOIN HardwareModels as models " +
+									"ON c.hardware_id=models.model_id " +
+									"INNER JOIN HardwareMake as make " +
+									"ON models.make_id=make.hardware_make_id " +
+									"INNER JOIN Software as soft " +
+									"ON c.software_id=soft.software_id " +
+									"INNER JOIN Solutions as solu " +
+									"ON prob.problem_id=solu.problem_id " +
+									"ORDER BY date_opened DESC"
+					var sql_make = "SELECT HardwareMake.hardware_make_name FROM HardwareMake"
+					var sql_model = "SELECT HardwareModels.model_name FROM HardwareModels"
+					var sql_software = "SELECT Software.software_name FROM Software"
+					var sql_desc = "SELECT Problem.problem_title FROM Problem"
+					// Could have allowed "multipleStatements" but this is generally unsafe to use as it exposes to sqlinjection
+
 					connection.query(sql_cases, function (err_cases, cases_data, fields_cases) {
 						if (err_cases) throw err_cases
-						console.log(cases_data)
-						res.render('selfhelp', { userName: username, userData: cases_data})
+						// console.log(cases_data)
+
+						connection.query(sql_make, function (err_makes, makes_data, fields_makes) {
+							if (err_makes) throw err_makes
+							// console.log(makes_data)
+
+							connection.query(sql_model, function (err_model, model_data, fields_model) {
+								if (err_model) throw err_model
+								// console.log(model_data)
+
+								connection.query(sql_desc, function (err_desc, desc_data, fields_desc) {
+									if (err_desc) throw err_desc
+									// console.log(desc_data)
+
+									connection.query(sql_software, function (err_softw, softw_data, fields_model) {
+										if (err_softw) throw err_softw
+										// console.log(softw_data)
+			
+										res.render('selfhelp', { userName: username, userData: cases_data, makes: makes_data, models: model_data, desc: desc_data, soft: softw_data})
+									});
+								});
+							});
+						});
 					});
 				}
+
+				// If specialist user signs in
 				if (usertype === 'specialist') {
 					res.render('specialist', { name: username })
 				}
